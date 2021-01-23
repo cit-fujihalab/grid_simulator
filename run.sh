a39cdd3912216eec9961bbd9616584d39d3f340a
Akihiro Fujihara <akihiro.fujihara@p.chibakoudai.jp>
2021-01-23 11:00:59 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bush
+#python simulator.py 10000 50 0.0
+#python simulator.py 10000 50 0.1
+#python simulator.py 10000 50 0.2
+python3 simulator.py 10000 50 0.25
+#python simulator.py 10000 50 0.3
+#python simulator.py 10000 50 0.4
+python3 simulator.py 500 500 0.5
+#python simulator.py 10000 50 0.6
+#python simulator.py 10000 50 0.7
+python3 simulator.py 500 500 0.75
+#python simulator.py 10000 50 0.8
+#python simulator.py 10000 50 0.9
+python3 simulator.py 500 500 1.0
